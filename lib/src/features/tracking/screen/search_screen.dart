dc973b76332b3c536edd904454afd0a33effac14
ashrafulparan2 <ashrafulparan@gmail.com>
2024-01-15 13:43:34 +0600

    mosque locator

--- lib/src/features/tracking/screen/search_screen.dart
+++ lib/src/features/tracking/screen/search_screen.dart
@@ -1,60 +1,65 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:lottie/lottie.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart' as GoogleMaps;
+import 'package:lottie/lottie.dart' as Lottie;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import '../../../../global/global.dart';
 
 class SearchScreen extends StatelessWidget {
-   SearchScreen();
+  SearchScreen();
 
-   getCurrentLiveLocationOfUser() async
-   {
-     Position positionOfUser = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.bestForNavigation);
-     currentPositionOfUser = positionOfUser;
-     GoogleMapController? controllerGoogleMap;
-
-
-     LatLng positionOfUserInLatLng = LatLng(currentPositionOfUser!.latitude, currentPositionOfUser!.longitude);
-
-     CameraPosition cameraPosition = CameraPosition(target: positionOfUserInLatLng, zoom: 15);
-     controllerGoogleMap!.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
-
-     // await CommonMethods.convertGeoGraphicCoOrdinatesIntoHumanReadableAddress(currentPositionOfUser!, context);
-
-     // await getUserInfoAndCheckBlockStatus();
-
-     // await initializeGeoFireListener();
-   }
-
-   final Completer<GoogleMapController> _controller =
-   Completer<GoogleMapController>();
-
-   static const CameraPosition _kGooglePlex = CameraPosition(
-     target: LatLng(37.42796133580664, -122.085749655962),
-     zoom: 14.4746,
-   );
-
-   static const CameraPosition _kLake = CameraPosition(
-       bearing: 192.8334901395799,
-       target: LatLng(37.43296265331129, -122.08832357078792),
-       tilt: 59.440717697143555,
-       zoom: 19.151926040649414);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        // mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        zoomControlsEnabled: true,
-        zoomGesturesEnabled: true,
-        mapType: MapType.normal,
-        myLocationEnabled: true,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+      body: FutureBuilder(
+        future: getCurrentLiveLocationOfUser(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return FutureBuilder(
+              future: getNearbyMosques(),
+              builder: (context, placesSnapshot) {
+                if (placesSnapshot.connectionState == ConnectionState.done) {
+                  return GoogleMaps.GoogleMap(
+                    initialCameraPosition: GoogleMaps.CameraPosition(
+                      target: GoogleMaps.LatLng(
+                        currentPositionOfUser!.latitude,
+                        currentPositionOfUser!.longitude,
+                      ),
+                      zoom: 15,
+                    ),
+                    zoomControlsEnabled: true,
+                    zoomGesturesEnabled: true,
+                    mapType: GoogleMaps.MapType.normal,
+                    myLocationEnabled: true,
+                    onMapCreated: (GoogleMaps.GoogleMapController controller) {
+                      _controller.complete(controller);
+                    },
+                    markers: Set.from(placesSnapshot.data!.map(
+                      (place) => GoogleMaps.Marker(
+                        markerId: GoogleMaps.MarkerId(place['place_id']),
+                        position: GoogleMaps.LatLng(
+                          place['geometry']['location']['lat'],
+                          place['geometry']['location']['lng'],
+                        ),
+                        infoWindow: GoogleMaps.InfoWindow(
+                          title: place['name'],
+                          snippet: place['vicinity'],
+                        ),
+                      ),
+                    )),
+                  );
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
         },
       ),
       floatingActionButton: FloatingActionButton.extended(
@@ -65,8 +70,40 @@ class SearchScreen extends StatelessWidget {
     );
   }
 
-   Future<void> _goToTheLake() async {
-     final GoogleMapController controller = await _controller.future;
-     await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-   }
+  Future<void> getCurrentLiveLocationOfUser() async {
+    Position positionOfUser = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.bestForNavigation,
+    );
+    currentPositionOfUser = positionOfUser;
+  }
+
+  Future<List<Map<String, dynamic>>> getNearbyMosques() async {
+    final apiKey =
+        'AIzaSyAz53HJ9y8znPBGmXlHvb3ic7uIJmr2Mdg'; // Replace with your API key
+    final radius = 5000; // Adjust the radius as needed
+    final url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
+        'location=${currentPositionOfUser!.latitude},${currentPositionOfUser!.longitude}'
+        '&radius=$radius&type=mosque&key=$apiKey';
+
+    final response = await http.get(Uri.parse(url));
+    final Map<String, dynamic> data = json.decode(response.body);
+
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+
+  final Completer<GoogleMaps.GoogleMapController> _controller =
+      Completer<GoogleMaps.GoogleMapController>();
+
+  static const GoogleMaps.CameraPosition _kLake = GoogleMaps.CameraPosition(
+    bearing: 192.8334901395799,
+    target: GoogleMaps.LatLng(37.43296265331129, -122.08832357078792),
+    tilt: 59.440717697143555,
+    zoom: 19.151926040649414,
+  );
+
+  Future<void> _goToTheLake() async {
+    final GoogleMaps.GoogleMapController controller = await _controller.future;
+    await controller
+        .animateCamera(GoogleMaps.CameraUpdate.newCameraPosition(_kLake));
+  }
 }
